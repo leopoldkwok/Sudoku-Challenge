f08b4260744ad8ec0e5ac0f23a37289a3651624e
leopoldkwok <leopoldkwok@gmail.com>
2015-02-04 23:38:46 +0000

    created initialization for cell

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,3 +1,12 @@
 class Cell
-	
+	attr_reader :value
+
+	def initialize(value)
+		@value = value
+	end
+
+	def solved?
+		@value != 0
+	end
+
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,13 +2,23 @@ require 'cell'
 
 describe Cell do 
 	context "on initialisation" do 
-		let(:value) {4}
+		let(:value) {5}
 		let(:cell) {Cell.new(value) }
 
 		it "should have a value of 5" do 
 			expect(cell.value).to eq (5)
-
 		end
 	end
 
+		let(:cell) { Cell.new(5)}
+
+		it "should know if it is solved" do
+			expect(cell.solved?).to be true
+		end
+
+		let(:empty_cell) { Cell.new(0)}
+
+		it "should know if it is not solved" do
+			expect(empty_cell.solved?).not_to be true
+		end
 end
